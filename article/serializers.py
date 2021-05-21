53113f160668b84fbc051143d08ea08d693c67c7
yaBorn <yaborn@foxmail.com>
2021-05-21 14:30:31 +0800

    超链接 分页

--- article/serializers.py
+++ article/serializers.py
@@ -8,14 +8,16 @@ from user_info.serializers import UserDescSerializer
 # 简化序列化器_使用ModelSerializer
 # 文章列表接口
 class ArticleListSerializer(serializers.ModelSerializer):
-    # 嵌套序列化器，嵌入了用户信息
-    # read_only 参数设置为只读
+    # 用户信息，嵌套序列化器
     author = UserDescSerializer(read_only=True)
+    # json中直接提供超链接地址
+    url = serializers.HyperlinkedIdentityField(view_name="article:detail")
+    # 这里的name为urls的name母urls中app对应的namespace
 
     class Meta:
         model = Article
         fields = [
-            'id',
+            'url',
             'title',
             'created',
             'author',
@@ -24,6 +26,9 @@ class ArticleListSerializer(serializers.ModelSerializer):
 
 # 文章详情接口
 class ArticleDetailSerializer(serializers.ModelSerializer):
+    # 嵌套用户信息
+    author = UserDescSerializer(read_only=True)
+
     class Meta:
         model = Article
         fields = '__all__'  # 使用所有字段
--- yaBorn_project/settings.py
+++ yaBorn_project/settings.py
@@ -41,6 +41,12 @@ INSTALLED_APPS = [
     'user_info',
 ]
 
+# 分页功能
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 2  # 设置每页文章数量
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
