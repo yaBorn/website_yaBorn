83eebafba90147e5d4577b35191730a2057d2294
yaBorn <yaborn@foxmail.com>
2021-06-08 22:07:41 +0800

    *重构_搜索框组件化

---
+++ frontend/src/components/my/Base.vue
@@ -0,0 +1,15 @@
+<!--
+    空 组件
+-->
+
+<!-- html -->
+<template>
+</template>
+
+<!-- js -->
+<script>
+</script>
+
+<!-- css -->
+<style>
+</style>
---
+++ frontend/src/components/my/SearchBox.vue
@@ -0,0 +1,15 @@
+<!--
+    搜索框 组件
+-->
+
+<!-- html -->
+<template>
+</template>
+
+<!-- js -->
+<script>
+</script>
+
+<!-- css -->
+<style>
+</style>
