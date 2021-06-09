f68eac1cd24fec26aa65102ac91ed879406b3e10
yaBorn <yaborn@foxmail.com>
2021-06-09 08:50:22 +0800

    用户中心_Header验证代码复用

--- frontend/src/components/BlogHeader.vue
+++ frontend/src/components/BlogHeader.vue
@@ -27,10 +27,18 @@
 
         <!-- 用户注册 -->
         <div class="login">
-            <!-- <p v-text="hasLogin"></p> -->
+            <!-- 已登录下拉框 -->
             <div v-if="hasLogin">
-                <!-- 已登录界面 -->
-                欢迎，{{username}}
+                <!-- <p v-text="hasLogin"></p> -->
+                <button class="dropbtn">
+                    欢迎，{{username}}
+                </button>
+                <!-- 下拉框内容 -->
+                <div class="dropdown-content">
+                    <router-link :to="{name:'UserCenter',params:{username:username}}">
+                        用户中心
+                    </router-link>
+                </div>
             </div>
             <div v-else>
                 <!-- 登录注册链接 -->
@@ -46,6 +54,7 @@
 <script>
     import axios from 'axios'
     import SearchBox from '@/components/SearchBox.vue'
+    import authorization from '@/func/authorization'
 
     export default {
         name: 'BlogHeader',
@@ -60,51 +69,57 @@
         },
         // 模板渲染 html后调用 二次操作 html的 dom结点
         mounted() {
-            const that = this
-            const storage = localStorage // login中获取的令牌数据
-            // 和 login-setItem名称相同
-            const expiredTime = Number(storage.getItem('expiredTime.myblog')) // 过期时间
-            const current = (new Date()).getTime() // 当前时间
-            const refreshToken = storage.getItem('refresh.myblog') // 刷新
-            that.username = storage.getItem('username.myblog') // 账号名称
-            console.log('-----header.vue')
-            console.log('当前时间:',current)
-            console.log('过期时间:',expiredTime)
-            console.log('账户：', that.username)
+            authorization()
+                .then(
+                    (data) => [this.hasLogin, this.username] = data
+                )
+            /*  // 未整合 func用户验证的代码
+                const that = this
+                const storage = localStorage // login中获取的令牌数据
+                // 和 login-setItem名称相同
+                const expiredTime = Number(storage.getItem('expiredTime.myblog')) // 过期时间
+                const current = (new Date()).getTime() // 当前时间
+                const refreshToken = storage.getItem('refresh.myblog') // 刷新
+                that.username = storage.getItem('username.myblog') // 账号名称
+                console.log('-----header.vue')
+                console.log('当前时间:',current)
+                console.log('过期时间:',expiredTime)
+                console.log('账户：', that.username)
 
-            // token未过期
-            if (expiredTime > current) {
-                that.hasLogin = true
-                console.log('令牌未过期，hasLogin：', that.hasLogin)
-            }
-            // token过期 但在可更新时间内
-            else if(refreshToken !== null){
-                // 重新申请token
-                console.log('令牌过期，重新申请')
-                axios
-                    .post('bg/token/refresh/', {
-                        refresh: refreshToken
-                    })
-                    .then(function (response) {
-                            // 刷新token
-                            const expiredTime = Date.parse(response.headers.date) + 60000;
-                            storage.setItem('access.myblog', response.data.access)
-                            storage.setItem('expiredTime.myblog', expiredTime)
-                            storage.removeItem('refresh.myblog')
-                            that.hasLogin = true
-                    })
-                    .catch(function () {
-                        // .clear() 清空当前域名下所有的值
-                        storage.clear()
-                        that.hasLogin = false
-                    })
-            }
-            // token过期 失效
-            else {
-                storage.clear()
-                that.hasLogin = false
-                console.log('令牌过期，失效，清空token，hasLogin:', that.hasLogin)
-            }
+                // token未过期
+                if (expiredTime > current) {
+                    that.hasLogin = true
+                    console.log('令牌未过期，hasLogin：', that.hasLogin)
+                }
+                // token过期 但在可更新时间内
+                else if(refreshToken !== null){
+                    // 重新申请token
+                    console.log('令牌过期，重新申请')
+                    axios
+                        .post('bg/token/refresh/', {
+                            refresh: refreshToken
+                        })
+                        .then(function (response) {
+                                // 刷新token
+                                const expiredTime = Date.parse(response.headers.date) + 60000;
+                                storage.setItem('access.myblog', response.data.access)
+                                storage.setItem('expiredTime.myblog', expiredTime)
+                                storage.removeItem('refresh.myblog')
+                                that.hasLogin = true
+                        })
+                        .catch(function () {
+                            // .clear() 清空当前域名下所有的值
+                            storage.clear()
+                            that.hasLogin = false
+                        })
+                }
+                // token过期 失效
+                else {
+                    storage.clear()
+                    that.hasLogin = false
+                    console.log('令牌过期，失效，清空token，hasLogin:', that.hasLogin)
+                }
+            */
         }
     }
 </script>
@@ -135,5 +150,53 @@
     }
     .login-link {
         color:black;
+    }
+</style>
+
+<!-- 拉按钮样式 -->
+<style scoped>
+    /* 来源: https://www.runoob.com/css/css-dropdowns.html */
+    .dropbtn {
+        background-color: mediumslateblue;
+        color: white;
+        padding: 8px 8px 30px 8px ;
+        font-size: 16px;
+        border: none;
+        cursor: pointer;
+        height: 16px;
+        border-radius: 5px;
+    }
+    /* 容器 <div> - 需要定位下拉内容 */
+    .dropdown {
+        position: relative;
+        display: inline-block;
+    }
+    /* 下拉内容 (默认隐藏) */
+    .dropdown-content {
+        display: none;
+        position: absolute;
+        background-color: #f9f9f9;
+        min-width: 120px;
+        box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+        text-align: center;
+    }
+    /* 下拉菜单的链接 */
+    .dropdown-content a {
+        color: black;
+        padding: 12px 16px;
+        text-decoration: none;
+        display: block;
+    }
+    /* 鼠标移上去后修改下拉菜单链接颜色 */
+    .dropdown-content a:hover {
+        background-color: #f1f1f1
+    }
+    /* 在鼠标移上去后显示下拉菜单 */
+    .dropdown:hover .dropdown-content {
+        display: block;
+    }
+    /* 当下拉内容显示后修改下拉按钮的背景颜色 */
+    .dropdown:hover .dropbtn {
+        background-color: darkslateblue;
     }
 </style>
