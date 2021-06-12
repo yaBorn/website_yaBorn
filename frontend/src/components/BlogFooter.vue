ddc3280d2a81dbc7e2c9ee8ad7726e05d125feba
yaBorn <yaborn@foxmail.com>
2021-06-12 21:00:01 +0800

    插件_doge

--- frontend/src/components/BlogFooter.vue
+++ frontend/src/components/BlogFooter.vue
@@ -5,18 +5,26 @@
 
 <!-- html -->
 <template>
+<div>
     <!-- 页脚 -->
     <router-link :to="{name:'Home'}"> <!-- 点击跳转回home -->
         <div id="footer">
             <p>yaborn.com</p>
         </div>
     </router-link>
+    <div id="doge">
+        <Doge/>
+    </div>
+</div>
 </template>
 
 <!-- js -->
 <script>
+import Doge from '@/components//others/Doge.vue'
+
     export default {
         name: 'BlogFooter',
+        components: {Doge}
     }
 </script>
 
